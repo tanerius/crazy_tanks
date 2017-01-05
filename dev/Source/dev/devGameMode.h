cec8bffbea8bd18a1280c510dd967cdd6b1ab2a8
Taner Selim <tanerius@gmail.com>
2017-01-06 00:22:25 +0100

    Unreal Project Opened

---
+++ dev/Source/dev/devGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "devGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEV_API AdevGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
