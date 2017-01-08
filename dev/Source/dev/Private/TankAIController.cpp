4a684df5f4ca4c00c680b84f97f09bb691b54c40
Taner Selim (Win) <tanerius@gmail.com>
2017-01-08 18:08:20 +0100

    AI finds the player controllers pawn now via c++

--- dev/Source/dev/Private/TankAIController.cpp
+++ dev/Source/dev/Private/TankAIController.cpp
@@ -14,14 +14,24 @@ void ATankAIController::BeginPlay()
     {
         UE_LOG(LogTemp, Warning, TEXT("AI not possesing a tank!"));
     }
-    //else
-    //{
-    //    UE_LOG(LogTemp, Warning, TEXT("AI possesing %s"), *possesedTank->GetName());
-    //}
+
+    ATank* thePlayer = GetPlayerTank();
+    if(!thePlayer)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("AI doesnt see player"));
+    }
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+    auto playerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+    if (!playerPawn) { return nullptr; }
+
+    return Cast<ATank>(playerPawn);
 }
 
--- dev/Source/dev/Public/TankAIController.h
+++ dev/Source/dev/Public/TankAIController.h
@@ -13,9 +13,13 @@ UCLASS()
 class DEV_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+private:
     // Returns the Tank that is being possessed by this AI controller.
     ATank* GetControlledTank() const;
+
+    // We need a method like this so that the AI can fint the player and target him
+    ATank* GetPlayerTank() const;
 
     void BeginPlay() override;
 	
