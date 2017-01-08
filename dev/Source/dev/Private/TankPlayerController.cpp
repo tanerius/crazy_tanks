2ddb102579569a7664cdc7f808130ab830eeebb7
Taner Selim (Win) <tanerius@gmail.com>
2017-01-08 20:22:55 +0100

    Found the X,Y screen location of the crosshair programatically

--- dev/Source/dev/Private/TankPlayerController.cpp
+++ dev/Source/dev/Private/TankPlayerController.cpp
@@ -41,6 +41,17 @@ ATank* ATankPlayerController::GetControlledTank() const
 bool ATankPlayerController::GetSightRayHitLocation(FVector& hl) const
 {
     hl = FVector(1.0f); // will set all components
+        
+    // Find the crosshair position
+    int32 viewportSizeX, viewportSizeY;
+    GetViewportSize(viewportSizeX, viewportSizeY);
+    FVector2D screenLocation = FVector2D( viewportSizeX * crosshairXLocation, viewportSizeY * crosshairYLocation );
+    // UE_LOG(LogTemp, Warning, TEXT("CrossHair screen location: %s"), *screenLocation.ToString());
+
+    // De-project screen position of the hrosshair to a world direction
+    // Line trace along that look direction and see what we hit (up to max range)
+
+
     return true;
 }
 
--- dev/Source/dev/Public/TankPlayerController.h
+++ dev/Source/dev/Public/TankPlayerController.h
@@ -14,18 +14,23 @@ class DEV_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 private:
+    UPROPERTY(EditAnywhere)
+    float crosshairXLocation = 0.5f;
+
+    UPROPERTY(EditAnywhere)
+    float crosshairYLocation = 0.33333f;
+
     // Start moving the tank barrel towards where the crosshair 
     // intersects the world - pretty complex stuff
     void AimTowardsCrosshair();
 
-    bool GetSightRayHitLocation(FVector& hl) const;
-
-public:
     // virtual - allows other children to override
     virtual void BeginPlay() override;
 
     // Returns the Tank that is being possessed by this player controller.
     ATank* GetControlledTank() const;
+
+    bool GetSightRayHitLocation(FVector& hl) const;
 
     // override the tick method so we can use it for aiming towards the crosshair
     // virtual - allows other children to override
